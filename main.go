8c0aadcda788700c0f661293446d63f9da381f05
Timothy Cole <tim@timcole.me>
2018-10-04 16:26:21 -0400

    Temp setup to get Tim's commands

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/TimothyCole/WeetBot-V2/twitch"
@@ -11,7 +14,51 @@ import (
 var (
 	nick = os.Getenv("TWITCH_BOT_NAME")
 	pass = os.Getenv("TWITCH_BOT_AUTH")
+	api  CommandsAPI
 )
+
+type CommandsAPI struct {
+	Data []Command `json:"data"`
+}
+
+type Command struct {
+	Channel   int    `json:"channel"`
+	Command   string `json:"command"`
+	Response  string `json:"response"`
+	Cooldown  int    `json:"cooldown"`
+	Userlevel int    `json:"userlevel"`
+	Points    int    `json:"points"`
+	Hidden    bool   `json:"hidden"`
+}
+
+func GetCommandsForTim() {
+	commands := "https://tcole.me/api/stream/51684790/commands"
+	req, _ := http.NewRequest("GET", commands, nil)
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	api = CommandsAPI{}
+	err = json.Unmarshal(body, &api)
+
+	api.Data = append(api.Data, Command{
+		Channel:   51684790,
+		Command:   "!ping",
+		Response:  "PONG KappaPride",
+		Cooldown:  0,
+		Userlevel: 0,
+		Points:    0,
+	})
+
+	fmt.Println("Commands Loaded for Tim")
+}
 
 func main() {
 	bot := twitch.NewClient(nick, pass)
@@ -19,8 +66,22 @@ func main() {
 		panic(err)
 	}
 
+	go GetCommandsForTim()
+
 	bot.OnNewMessage(func(msg *twitch.Message) {
 		fmt.Println("> New Message: ", msg.Data.DisplayName, msg.Data.Message)
+
+		if "!reload" == msg.Data.Message && msg.Data.DisplayName == "ModestTim" {
+			go GetCommandsForTim()
+		}
+
+		for _, command := range api.Data {
+			if command.Command != msg.Data.Message || command.Channel != msg.Data.StreamerID {
+				continue
+			}
+
+			bot.Say(msg.Data.StreamerName, command.Response)
+		}
 	})
 	bot.OnNewWhisper(func(msg *twitch.Message) {
 		fmt.Println("> New Whisper: ", msg.Data.DisplayName, msg.Data.Message)
@@ -36,7 +97,7 @@ func main() {
 	})
 
 	go joinChannel(bot, 51684790, "ModestTim")
-	go joinChannel(bot, 41245072, "LoserFruit")
+	go joinChannel(bot, 88560344, "WeetBot")
 
 	<-bot.Done
 }
